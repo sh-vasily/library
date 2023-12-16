bc404a8502a3eae636731c94e1dbfbb32af39237
Василий Щербаков <basil-scherbakov@ya.ru>
2023-12-16 23:59:35 +0300

    simplify code

--- Library.Mvvm/Command/RelayCommand.cs
+++ Library.Mvvm/Command/RelayCommand.cs
@@ -6,22 +6,11 @@ public sealed class RelayCommand(
     Action<object> execute,
     Predicate<object> canExecute = null) : ICommand
 {
-    public bool CanExecute(object parameter)
-    {
-        return canExecute?.Invoke(parameter) ?? true;
-    }
-
-    public void Execute(object parameter)
-    {
-        execute(parameter);
-    }
-
+    public bool CanExecute(object parameter) => canExecute?.Invoke(parameter) ?? true;
+    public void Execute(object parameter) => execute(parameter);
     public event EventHandler CanExecuteChanged
     {
-        add =>
-            CommandManager.RequerySuggested += value;
-
-        remove =>
-            CommandManager.RequerySuggested -= value;
+        add => CommandManager.RequerySuggested += value;
+        remove => CommandManager.RequerySuggested -= value;
     }
 }
