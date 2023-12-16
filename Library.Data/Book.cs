946a862b9a38a81f8992bcf665418effb63a7d61
Василий Щербаков <basil-scherbakov@ya.ru>
2023-12-17 00:01:06 +0300

    add isbn

--- Library.Data/Book.cs
+++ Library.Data/Book.cs
@@ -5,6 +5,8 @@ public class Book
     public int Id { get; set; }
     public string Title { get; set; }
     
+    public string ISBN { get; set; }
+    
     public virtual List<Author> Authors { get; set; }
 }
 
